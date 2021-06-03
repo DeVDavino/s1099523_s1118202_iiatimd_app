a2adc3033a717191216956a2dd93e02b75c763b1
DeVDavino <doubled.rosaria@gmail.com>
2021-06-03 20:19:20 +0200

    android files toegevoegd

---
+++ app/src/main/java/com/example/iatiimd_eindoplevering/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iatiimd_eindoplevering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
