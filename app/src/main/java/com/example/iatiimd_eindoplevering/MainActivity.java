bc22ea67ad280f4ab02093da448d5ffa21528d09
Davino Rosaria <doubled.rosaria@gmail.com>
2021-06-27 15:55:56 +0200

    fraction update

--- app/src/main/java/com/example/iatiimd_eindoplevering/MainActivity.java
+++ app/src/main/java/com/example/iatiimd_eindoplevering/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.iatiimd_eindoplevering;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +23,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button toIdeeScreen = findViewById(R.id.button_idee);
-        toIdeeScreen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openIdee();
-            }
-        });
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container, new HomeFragment()).commit();
+//        toIdeeScreen.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openIdee();
+//            }
+//        });
     }
+
+    public BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.navigation_idee:
+                            selectedFragment = new DashboardFragment();
+                            break;
+
+                        case R.id.navigation_profile:
+                            selectedFragment = new NotificationFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+
+                }
+    };
 
     public void openIdee() {
         Bundle bundleIdee = new Bundle();
