a24774efd1777c8189b7b6486a45c76b70d48d6e
Davino Rosaria <doubled.rosaria@gmail.com>
2021-07-05 17:31:57 +0200

    initial push

--- app/src/main/java/com/example/iatiimd_eindoplevering/ProfileFragment.java
+++ app/src/main/java/com/example/iatiimd_eindoplevering/ProfileFragment.java
@@ -1,12 +1,35 @@
 package com.example.iatiimd_eindoplevering;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.iatiimd_eindoplevering.RestAPI.ApiClient;
+import com.example.iatiimd_eindoplevering.RestAPI.ApiGetService;
+import com.example.iatiimd_eindoplevering.RestAPI.ApiGetUser;
+import com.example.iatiimd_eindoplevering.RestAPI.tokenResponse;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +37,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-
+    private Button loguit;
+    private TextView name_tv,email_tv;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -60,5 +84,64 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        loguit = (Button) getView().findViewById(R.id.uitloggen);
+        name_tv = (TextView) getView().findViewById(R.id.name);
+        email_tv = (TextView) getView().findViewById(R.id.email);
+
+        loguit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+
+        });
+        getUser();
+    }
+    private void logout(){
+        tokenResponse tokenResponse = (tokenResponse) getActivity().getApplicationContext();
+        tokenResponse.setAccess_token(null);
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void getUser(){
+        tokenResponse tokenResponse = (tokenResponse) getActivity().getApplicationContext();
+        final String token = "Bearer " + tokenResponse.getAccess_token();
+
+        ApiGetUser service = ApiClient.getClient().create(ApiGetUser.class);
+        Call<ResponseBody> srvUser = service.getUser(token);
+
+        srvUser.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()){
+                    try {
+                        String answer = response.body().string();
+                        Log.d("antwoord", answer);
+
+                        JSONObject user = new JSONObject(answer);
+                        name_tv.setText(user.getString("name"));
+                        email_tv.setText(user.getString("email"));
+
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 }
